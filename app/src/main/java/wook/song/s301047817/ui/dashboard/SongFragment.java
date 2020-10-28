8e53c676a02e8833b1c0b35d02d51ff80988da79
Song Wook <songtri@gmail.com>
2020-10-27 22:46:13 -0400

    complete song fragment

--- app/src/main/java/wook/song/s301047817/ui/dashboard/SongFragment.java
+++ app/src/main/java/wook/song/s301047817/ui/dashboard/SongFragment.java
@@ -3,12 +3,19 @@ package wook.song.s301047817.ui.dashboard;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -23,12 +30,15 @@ public class SongFragment extends Fragment {
     private SongViewModel dashboardViewModel;
     private final int REQUEST_CODE = 1234;
     private final int TAKE_PICTURE = 1;
+    ArrayAdapter spinnerAnimPlayTimeAdapter;
+    private float animPlayTime;
+    AnimationDrawable mframeAnimation = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(SongViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_song, container, false);
+        final View root = inflater.inflate(R.layout.fragment_song, container, false);
 
         Button takePictureButton = root.findViewById(R.id.wookTakePicture);
         takePictureButton.setOnClickListener(new View.OnClickListener() {
@@ -43,10 +53,46 @@ public class SongFragment extends Fragment {
                         return;
                     }
                     else
-                        TakeCamera();
+                        takeCamera();
                 }
             }
         });
+
+        Button animStartButton = root.findViewById(R.id.wookBtnAniStart);
+        animStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAnimation();
+            }
+        });
+        Button animStopButton = root.findViewById(R.id.wookBtnAniStop);
+        animStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopAnimation();
+            }
+        });
+
+        Spinner penThickness = root.findViewById(R.id.wookSpinnerAnimPlayTime);
+        spinnerAnimPlayTimeAdapter = ArrayAdapter.createFromResource(getContext(), R.array.wookAniPlayTime, android.R.layout.simple_spinner_dropdown_item);
+        penThickness.setAdapter(spinnerAnimPlayTimeAdapter);
+        penThickness.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String value = (String)spinnerAnimPlayTimeAdapter.getItem(i);
+                animPlayTime = Float.parseFloat(value);
+                buildAnimation(root, animPlayTime);
+                Log.i("WookLab3", value);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                animPlayTime = 1;
+            }
+        });
+
+        buildAnimation(root, 1f);
+
         return root;
     }
 
@@ -57,7 +103,7 @@ public class SongFragment extends Fragment {
         switch (requestCode) {
             case REQUEST_CODE:
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    TakeCamera();
+                    takeCamera();
                 }
                 break;
             default:
@@ -65,8 +111,52 @@ public class SongFragment extends Fragment {
         }
     }
 
-    private void TakeCamera() {
+    private void takeCamera() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(takePictureIntent, TAKE_PICTURE);
+    }
+
+    private void buildAnimation(View view, float animPlayTime) {
+        ImageView img = (ImageView)view.findViewById(R.id.wookAnimationImage);
+
+        BitmapDrawable frame1 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim1);
+        BitmapDrawable frame2 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim2);
+        BitmapDrawable frame3 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim3);
+        BitmapDrawable frame4 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim4);
+        BitmapDrawable frame5 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim5);
+        BitmapDrawable frame6 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim6);
+        BitmapDrawable frame7 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim7);
+        BitmapDrawable frame8 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim8);
+        BitmapDrawable frame9 = (BitmapDrawable)getResources().getDrawable(R.drawable.anim9);
+
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        int frameDuration = (int)(animPlayTime * 1000f / 9f);
+        mframeAnimation = new AnimationDrawable();
+        mframeAnimation.setOneShot(false);	// loop continuously
+        mframeAnimation.addFrame(frame1, frameDuration);
+        mframeAnimation.addFrame(frame2, frameDuration);
+        mframeAnimation.addFrame(frame3, frameDuration);
+        mframeAnimation.addFrame(frame4, frameDuration);
+        mframeAnimation.addFrame(frame5, frameDuration);
+        mframeAnimation.addFrame(frame6, frameDuration);
+        mframeAnimation.addFrame(frame6, frameDuration);
+        mframeAnimation.addFrame(frame7, frameDuration);
+        mframeAnimation.addFrame(frame8, frameDuration);
+        mframeAnimation.addFrame(frame9, frameDuration);
+
+        img.setBackground(mframeAnimation);
+    }
+
+    private void startAnimation() {
+        if(!mframeAnimation.isRunning()) {
+            Log.i("WookLab3", "animation start");
+            mframeAnimation.setVisible(true, true);
+            mframeAnimation.start();
+        }
+    }
+
+    private void stopAnimation() {
+        Log.i("WookLab3", "animation stop");
+        mframeAnimation.stop();
     }
 }
---
+++ app/src/main/java/wook/song/s301047817/ui/home/wookCanvasView.java
@@ -0,0 +1,98 @@
+package wook.song.s301047817.ui.home;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/* Wook Song, 301047817, Section 2 */
+public class wookCanvasView extends View {
+
+    private static final float TOLERANCE = 5;
+    private Path mPath;
+    private Paint mPaint;
+
+    private float mX, mY;
+
+    public wookCanvasView(Context c, AttributeSet attrs) {
+        super(c, attrs);
+
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5f);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    private void startTouch(float x, float y) {
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    // when ACTION_MOVE move touch according to the x,y values
+    private void moveTouch(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOLERANCE || dy >= TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    private void upTouch() {
+        mPath.lineTo(mX, mY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                upTouch();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    public void clear() {
+        mPath.reset();
+        invalidate();
+    }
+
+    public void setPenAttribute(int color, int thickness) {
+        mPaint.setColor(color);
+        mPaint.setStrokeWidth(thickness);
+    }
+}
