112b582f8338b70e1d8150699f77e7412c5f771d
Song Wook <songtri@gmail.com>
2020-10-27 21:30:08 -0400

    add take camera function

--- app/src/main/java/wook/song/s301047817/ui/dashboard/SongFragment.java
+++ app/src/main/java/wook/song/s301047817/ui/dashboard/SongFragment.java
@@ -1,15 +1,18 @@
 package wook.song.s301047817.ui.dashboard;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import wook.song.s301047817.R;
@@ -18,12 +21,52 @@ import wook.song.s301047817.R;
 public class SongFragment extends Fragment {
 
     private SongViewModel dashboardViewModel;
+    private final int REQUEST_CODE = 1234;
+    private final int TAKE_PICTURE = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(SongViewModel.class);
         View root = inflater.inflate(R.layout.fragment_song, container, false);
+
+        Button takePictureButton = root.findViewById(R.id.wookTakePicture);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hasPermission = 0;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    hasPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA);
+
+                    if (hasPermission != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
+                        return;
+                    }
+                    else
+                        TakeCamera();
+                }
+            }
+        });
         return root;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    TakeCamera();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void TakeCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, TAKE_PICTURE);
     }
 }
