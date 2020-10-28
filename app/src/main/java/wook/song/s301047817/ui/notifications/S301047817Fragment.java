12d92c959bbfcb1892da0729275436b1a83d317d
Song Wook <songtri@gmail.com>
2020-10-27 23:19:59 -0400

    add tween anim

--- app/src/main/java/wook/song/s301047817/ui/notifications/S301047817Fragment.java
+++ app/src/main/java/wook/song/s301047817/ui/notifications/S301047817Fragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,13 +26,17 @@ public class S301047817Fragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(S301047817ViewModel.class);
         View root = inflater.inflate(R.layout.fragment_s301047817, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final TextView textView = root.findViewById(R.id.wookTweenText);
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.rotation);
+        textView.startAnimation(anim);
+
         return root;
     }
 }
